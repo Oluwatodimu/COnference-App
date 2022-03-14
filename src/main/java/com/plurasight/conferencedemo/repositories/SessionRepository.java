7b90f2614d244317c44500228bdcc2ca04277865
Todimu Isewon <81085914+Oluwatodimu@users.noreply.github.com>
2022-03-14 07:55:09 +0100

    Add files via upload

---
+++ src/main/java/com/plurasight/conferencedemo/repositories/SessionRepository.java
@@ -0,0 +1,8 @@
+package com.plurasight.conferencedemo.repositories;
+
+import com.plurasight.conferencedemo.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+}
